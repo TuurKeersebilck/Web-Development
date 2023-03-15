7ec8789f446f14ec24c35a8fcef0962cb840b35c
MalleTuur <tuur.k@hotmail.com>
2023-03-15 13:59:51 +0100

    web dev

--- tweede_semester/Labo_4/opdracht_an/scripts/index.js
+++ tweede_semester/Labo_4/opdracht_an/scripts/index.js
@@ -1,13 +1,12 @@
 const setup = () => {
 	const zin = "De man van An geeft geen hand aan ambetante verwanten";
-	const deelTekst = "an";
 	let telling = 0;
   
 	let index = zin.indexOf(deelTekst);
   
 	while (index !== -1) {
 	  telling++;
-	  index = zin.toLowerCase().indexOf(deelTekst, index + 1);
+	  index = zin.toLowerCase().indexOf('an', index + 1);
 	}
 	console.log(`Het deel an komt zoveel keer voor:${telling}`);
   }
