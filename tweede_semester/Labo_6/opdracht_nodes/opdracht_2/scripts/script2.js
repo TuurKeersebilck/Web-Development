e38e943f99c3b8129b7831b14bfbc253b0927c58
MalleTuur <tuur.k@hotmail.com>
2023-03-22 16:58:42 +0100

    web dev

--- tweede_semester/Labo_6/opdracht_nodes/opdracht_2/scripts/script2.js
+++ tweede_semester/Labo_6/opdracht_nodes/opdracht_2/scripts/script2.js
@@ -2,9 +2,13 @@ const setup = () => {
 
   const listItems = document.querySelectorAll('li');
 
+  const style = document.createElement('style');
+  style.textContent = '.listitem { color: red; }';
+  document.head.appendChild(style);
+  
+
   listItems.forEach(item => {
     item.className = 'listitem';
-    item.style.color = 'red';
   });
 
   const afbeelding = document.createElement('img');
