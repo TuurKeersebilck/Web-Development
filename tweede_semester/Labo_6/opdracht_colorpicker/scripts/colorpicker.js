e72d88b53b5189fa35d99bf06d48dabe9747ed2a
MalleTuur <tuur.k@hotmail.com>
2023-03-25 12:02:26 +0100

    Web dev

--- tweede_semester/Labo_6/opdracht_colorpicker/scripts/colorpicker.js
+++ tweede_semester/Labo_6/opdracht_colorpicker/scripts/colorpicker.js
@@ -1,31 +1,49 @@
 const setup = () => {
     const sliders = document.querySelectorAll(".slider");
-
+    const colorPicker = document.querySelector("#color-picker");
+  
     sliders.forEach(slider => {
-        slider.addEventListener("change", update);
-        slider.addEventListener("input", update);
+      slider.addEventListener("change", update);
+      slider.addEventListener("input", update);
     });
     update();
-
-    const saveSwatch = document.querySelectorAll('button');
-    saveSwatch.addEventListener("click", saveSwatch);
-}
-
-
-const update = () => {
-    const sliders = document.getElementsByClassName("slider"); /*COLLECTIE V/D BALKEN*/
-    const pTags = document.getElementsByTagName("p"); /*TEKST EN WAARDEN VERANDEREN VAN DE KLEUREN*/
-    const swatch = document.getElementsByClassName("swatch");  /*BLOK KLEUR*/
-
+  
+    //-----SWATCH TOEVOEGEN EN VERWIJDEREN
+    const saveSwatch = document.querySelector('button');
+    saveSwatch.addEventListener("click", () => {
+  
+      const nieuweSwatch = document.createElement("div");
+      nieuweSwatch.classList.add("swatchSave");
+      nieuweSwatch.style.backgroundColor = `rgb(${sliders[0].value}, ${sliders[1].value}, ${sliders[2].value})`;
+  
+      const deleteKnop = document.createElement("button");
+      deleteKnop.classList.add("delete");
+      deleteKnop.textContent = "X";
+      deleteKnop.addEventListener("click", () => {
+        nieuweSwatch.remove();
+      });
+  
+      nieuweSwatch.appendChild(deleteKnop);
+      document.body.appendChild(nieuweSwatch);
+    });
+  }
+  
+  //-----ORIGINELE CODE
+  const update = () => {
+    const sliders = document.getElementsByClassName("slider");
+    const pTags = document.getElementsByTagName("p");
+    const swatch = document.getElementsByClassName("swatch");
+  
     const rood = sliders[0].value;
     const groen = sliders[1].value;
     const blauw = sliders[2].value;
-
-    pTags[0].innerText = "Red " + rood;
-    pTags[1].innerText = "Green " + groen;
-    pTags[2].innerText = "Blue " + blauw;
-
+  
+    pTags[0].textContent = "Red " + rood;
+    pTags[1].textContent = "Green " + groen;
+    pTags[2].textContent = "Blue " + blauw;
+  
     swatch[0].style.backgroundColor = `rgb(${rood}, ${groen}, ${blauw})`;
-}
-
-window.addEventListener("load", setup);
+  }
+  
+  window.addEventListener("load", setup);
+  
